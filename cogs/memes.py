dabe1dc0507d5ce3c7aa944617c87a5880102fb5
mentallydetached <mental.torture@gmail.com>
2021-07-12 21:14:36 -0500

    Initial commit

---
+++ cogs/memes.py
@@ -0,0 +1,22 @@
+from discord.ext import commands
+import random
+import discord
+import os
+
+memePics = []
+for filename in os.listdir('cogs/memes'):
+    memePics.append(f'cogs/memes/{filename}')
+
+
+class Images(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(aliases=["m", "randommeme", "random_meme", "memes"])
+    async def memes(self, ctx):
+        """Fulfills your life with a random meme."""
+        await ctx.send(file=discord.File(random.choice(memePics)))
+
+
+def setup(bot):
+    bot.add_cog(Images(bot))
